651b87ac98c3f4ab42b26cd1b1fb7f3cb0eb50c5
YanMer1 <mer21002@byui.edu>
2023-10-28 23:53:15 +0100

    Commit

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'My API',
     description: 'Anime API'
   },
-  host: 'https://animeproject.onrender.com',
+  host: 'animeproject.onrender.com',
   schemes: ['https']
 };
 
